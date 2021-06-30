9d41868dba7c3dd96baacc65d8d054c2242dc3f8
Zezhou Zheng <zezhou.zheng@shopee.com>
2021-06-30 13:55:54 +0800

    remove dao layer and use django models

--- productApp/service.py
+++ productApp/service.py
@@ -6,7 +6,15 @@ import dao
 
 def product_service(name, category, last_updated_at, limit):
     print("inner params [%s || %s || %d || %d]" % (name, category, last_updated_at, limit))
-    data = dao.query_product_list(name, category, last_updated_at, limit)
+    # data = dao.query_product_list(name, category, last_updated_at, limit)
+    objs = models.ProductTab.objects
+    if name:
+        objs = objs.filter(name=name)
+    if category:
+        objs = objs.filter(category=category)
+    objs = objs.exclude(updated_at__lt=last_updated_at)
+    data = objs.order_by("updated_at")
+    print(data)
     return data
 
 
