0e47299ead4a376660ff6d3397be51e848d9a006
Zezhou Zheng <zezhou.zheng@shopee.com>
2021-07-05 16:02:36 +0800

    python cache

--- user/views.py
+++ user/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 
 from user import register, login
 
+cache = {}
 
 def user_register(request):
     name = "user" + str(time.time())
@@ -29,10 +30,17 @@ def user_login(request):
     name = "user"
     if "name" in request.GET.keys():
         name = request.GET["name"]
-    password = "password"
-    if "password" in request.GET.keys():
-        password = request.GET["password"]
-    data = login.user_login(name, password)
+
+    if name in cache.keys():
+        data = cache[name]
+        print("cached")
+    else:
+        password = "password"
+        if "password" in request.GET.keys():
+            password = request.GET["password"]
+        data = login.user_login(name, password)
+        cache[name] = data
+        print("not cached, set cache")
 
     # todo: format data
     login_payload = {
