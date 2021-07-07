1594a6909fd5bb9c4c4830ffc03826df0772d8a3
Zezhou Zheng <zezhou.zheng@shopee.com>
2021-07-07 22:32:32 +0800

    disable debug mode

--- productProject/settings.py
+++ productProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c7l9*d6(6tct4ap2wyet$vo+e)4m-2sy4eykuefz6rp0myca39'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["10.143.143.19", "10.143.143.174", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
