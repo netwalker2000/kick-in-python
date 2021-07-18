7e2ae41bb7f667dbee4241439cc013a6e4b463f8
vincent <netwalker2000@hotmail.com>
2021-07-18 21:16:46 +0800

    fix the leak

--- productProject/settings.py
+++ productProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c7l9*d6(6tct4ap2wyet$vo+e)4m-2sy4eykuefz6rp0myca39'
+SECRET_KEY = 'frgregfgdfgfd#$%^&*(fgdsfsafsafafa^&(*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
